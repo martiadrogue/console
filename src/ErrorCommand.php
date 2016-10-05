5bc25a12833f17e28b3c5e1a1314444d1384e421
Martí Adrogué <marti.adrogue@gmail.com>
2016-10-05 19:44:27 +0200

    Create commands to handle emergencies

---
+++ src/ErrorCommand.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * This file is part of Console package.
+ *
+ * (c) Martí Adrogué <marti.adrogue@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace MartiAdrogue\Console;
+
+use MartiAdrogue\Console\Command;
+
+class ErrorCommand extends Command
+{
+    public function configure()
+    {
+    }
+
+    public function execute(array $arguments, Output $output)
+    {
+    }
+}
