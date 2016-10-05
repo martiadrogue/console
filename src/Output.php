a13182b77035d12e331f8a043bd4f45d819bf3a0
Martí Adrogué <marti.adrogue@gmail.com>
2016-10-05 19:47:36 +0200

    Create console output

---
+++ src/Output.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * This file is part of Console package.
+ *
+ * (c) Martí Adrogué <marti.adrogue@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace MartiAdrogue\Console;
+
+class Output
+{
+    public function print()
+    {
+    }
+}
